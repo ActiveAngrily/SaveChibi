dea737edd9556f8c43761170f706ee99bf844d68
Anant1910 <Anant1910@users.noreply.github.com>
2020-07-11 14:03:33 +0530

    =Added CarControl, Added GameMaster Script, Added Close Call Script, Created Trigger for CloseCall, Created ScoreSystems

---
+++ Out Of Control/Assets/Scripts/CarController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float speed = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * speed;
+    }
+
+    // Run animations when animations are done
+}
---
+++ Out Of Control/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public int score = 0;
+    public int closeCallMultiplier = 1;
+
+    float nextTime = 0f;
+    float multiplierCancelTime = 5f;
+    
+    public Text scoreUI;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if(Time.time > nextTime)
+        {
+            closeCallMultiplier = 1;
+        }
+        scoreUI.text =  score.ToString();
+    }
+
+    public void CloseCall()
+    {
+        //if the dog almost dodges the car, it will give extra points
+        score += 100 * closeCallMultiplier;
+        nextTime = Time.time + multiplierCancelTime;
+    }
+}
