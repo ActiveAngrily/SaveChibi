2081e45e8368d8efb58eb5fb13ffc2f025876961
Anant1910 <Anant1910@users.noreply.github.com>
2020-07-11 17:35:05 +0530

    1 - Updated Splash Screen

--- Out Of Control/Assets/Scripts/GameMaster.cs
+++ Out Of Control/Assets/Scripts/GameMaster.cs
@@ -5,28 +5,17 @@ using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
-    /*
-    public int score = 0;
-    public int closeCallMultiplier = 1;
-
-    float nextTime = 0f;
-    float multiplierCancelTime = 5f;
-    
-    public Text scoreUI;
-
-    private void Start()
-    {
-    }
 
     private void Update()
     {
-        if(Time.time > nextTime)
-        {
-            closeCallMultiplier = 1;
-        }
-        scoreUI.text =  score.ToString();
+
     }
-*/
+
+    private void DialogueTimer()
+    {
+        
+    }
+
     public void CloseCall()
     {
         //if the dog almost dodges the car, it will give extra points
