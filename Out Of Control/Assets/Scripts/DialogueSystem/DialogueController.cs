ff14a0e3bc43febcf320c67f01d6de8b579963fa
Anant1910 <Anant1910@users.noreply.github.com>
2020-07-11 22:21:28 +0530

    Update, Fixed Bug, Added Dialogue System

--- Out Of Control/Assets/Scripts/DialogueSystem/DialogueController.cs
+++ Out Of Control/Assets/Scripts/DialogueSystem/DialogueController.cs
@@ -10,9 +10,24 @@ public class DialogueController : MonoBehaviour
 
     private Queue<string> sentences;
 
+    public Dialogue dialogue;
+
+    float nextTime = 0f;
+    public float waitTime = 5f;
     void Start()
     {
         sentences = new Queue<string>();
+        StartDialogue(dialogue);
+    }
+
+    private void Update()
+    {
+        if (Time.time >= nextTime)
+        {
+            NextSentence();
+            nextTime = Time.time + waitTime;
+            
+        }
     }
 
     public void StartDialogue(Dialogue dialogue)
@@ -25,7 +40,9 @@ public class DialogueController : MonoBehaviour
             sentences.Enqueue(sentence);
         }
 
-        NextSentence();
+        nextTime = Time.time;
+
+
     }
 
     public void NextSentence()
@@ -36,13 +53,17 @@ public class DialogueController : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
-
     private IEnumerator TypeSentence(string sentence)
     {
         dialogueText.text = "";
+
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
+            /*for(int i = 0; i < 8; i++)
+            {
+                yield return null;
+            }*/
             yield return null;
         }
     }
