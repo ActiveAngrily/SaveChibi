7114d58879add25c7eb5e1a810e15d78daad0c20
Josh <joshharvey@btopenworld.com>
2020-07-12 15:26:50 +0100

    Control bar

---
+++ Out Of Control/Assets/controlBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class controlBar : MonoBehaviour {
+    public Sprite mouse;
+    public Sprite wasd;
+    public Sprite unknown;
+
+    public void Mouse() {
+        enabled = true;
+        GetComponent<Image>().sprite = mouse;
+    }
+
+    public void WASD() {
+        enabled = true;
+        GetComponent<Image>().sprite = wasd;
+    }
+    
+    public void Unknown() {
+        enabled = true;
+        GetComponent<Image>().sprite = unknown;
+    }
+
+    public void Disable() {
+        enabled = false;
+    }
+}
